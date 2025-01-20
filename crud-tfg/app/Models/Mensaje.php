c01bb38fd5e799dc82ff9c8172fa4296c7255f57
Pilar <mpagval998@g.educaand.es>
2025-01-20 18:34:00 +0100

    ActModelMensaje.20.1.25

--- crud-tfg/app/Models/Mensaje.php
+++ crud-tfg/app/Models/Mensaje.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Mensaje extends Model
 {
     /**
+     * 
+     * Esta clase es un modelo de Eloquent en Laravel lo que significa
+     * que representa una tabla en la bbdd y da métodos para interactuar
+     * con ella.
      * Los atributos que son asignables en masa.
      *
      * @var array
@@ -16,12 +20,15 @@ class Mensaje extends Model
         'receptor_id',
         'contenido',
     ];
-
+//@fillable es un array que define los atributos que se pueden asignar en masa.
     public function emisor()
     {
         return $this->belongsTo(NuevosAlumnos::class, 'emisor_id');
     }
-
+//emisor: es una función que define una relación de tipo belongsTo con 
+//el modelo NuevosAlumnos, esto significa que un mensaje tiene un 
+//emisor que es un registro en la tabla de NuevosAlumnos. La FK en la 
+//tabla mensajes es emisor_id.
     public function receptor()
     {
         return $this->belongsTo(NuevosAlumnos::class, 'receptor_id');
